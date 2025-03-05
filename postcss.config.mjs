712cf79ebe7fb49fc1fadc09a4863bd06787c1e7
Quentin <quentindumon352a@gmail.com>
2025-03-05 17:01:36 +0100

    feat(NavBar): create component

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,10 @@
+
+
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {
+      'config': './tailwind.config.js',
+    },
   },
 };
 export default config;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,40 @@
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
+
 export default function NavBar() {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const openModal = () => setIsModalOpen(true)
+	const closeModal = () => setIsModalOpen(false)
+
 	return (
-		<div className='navbar bg-base-100 shadow-sm primary'>
+		<div className='navbar bg-base-100 shadow-sm bg-primary'>
+			<div>
+				<Image src='/Pokeball.svg' alt='Pokeball' width={35} height={35} />
+			</div>
 			<div className='flex-1'>
-				<a className='btn btn-ghost text-xl'>Ma liste de BG DaisyUI</a>
+				<a className='btn btn-ghost text-xl'>Poke-dex</a>
 			</div>
 			<div className='flex-none'>
-				<ul className='menu menu-horizontal px-1'>
-					<li>
-						<a>Link</a>
-					</li>
-					<li>
-						<details>
-							<summary>Parent</summary>
-							<ul className='bg-base-100 rounded-t-none p-2'>
-								<li>
-									<a>Link 1</a>
-								</li>
-								<li>
-									<a>Link 2</a>
-								</li>
-							</ul>
-						</details>
-					</li>
-				</ul>
+				<button className='btn rounded-full' onClick={openModal}>
+					#
+				</button>
+				{isModalOpen && (
+					<dialog open className='modal'>
+						<div className='modal-box'>
+							<h3 className='font-bold text-lg'>Hello!</h3>
+							<p className='py-4'>
+								Press ESC key or click the button below to close
+							</p>
+							<div className='modal-action'>
+								<button className='btn' onClick={closeModal}>
+									Close
+								</button>
+							</div>
+						</div>
+					</dialog>
+				)}
 			</div>
 		</div>
 	)
